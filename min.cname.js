272cc110488cb00e4c8d48c9722347e77ddc5f69
oroafrica <52736386+oroafrica@users.noreply.github.com>
2020-04-15 15:38:56 +0200

    target input update two

--- min.cname.js
+++ min.cname.js
@@ -13,7 +13,7 @@ class Product
         this.ns = "http://www.w3.org/2000/svg";
         this._title = $(document).find("title").text();
 		
-		this.txt = $("input:text").get(1);
+		this.txt = $('*[placeholder="Max 10 characters with first in Caps"]');
 		this.selOne = $("select").get(0);
 		this.selTwo = $("select").get(1);
     }
