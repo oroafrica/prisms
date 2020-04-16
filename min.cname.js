b4b38c1c64c48d83e6c2e8bb712dcc03d2ad5b2a
oroafrica <52736386+oroafrica@users.noreply.github.com>
2020-04-16 12:41:40 +0200

    b34

--- min.cname.js
+++ min.cname.js
@@ -13,7 +13,7 @@ class Product
         this.ns = "http://www.w3.org/2000/svg";
         this._title = $(document).find("title").text();
 		
-				this.txt = null;
+				this.txt =  $("*[title='Max 10 characters with first in Caps']");
 				this.selOne = null;
 				this.selTwo = null;
     }
